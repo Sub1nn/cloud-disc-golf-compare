6707c7dd3906be840667520337e205f8eecfed8f
Rihards Gailis <Rihards.Gailis@student.lut.fi>
2024-10-30 00:09:56 +0200

    DGEPC-2
    DGEPC-2

--- app/handle_credentials.py
+++ app/handle_credentials.py
@@ -1,6 +1,8 @@
 
 import os
+
 from dotenv import load_dotenv
+from google.cloud import secretmanager
 
 def get_secret(secret_name):
 
@@ -9,6 +11,12 @@ def get_secret(secret_name):
     if app_env == 'local':
         load_dotenv() 
         return os.getenv(secret_name)
+    
+    elif app_env == 'prod':
+        client = secretmanager.SecretManagerServiceClient()
+        name = f"projects/durable-path-439213-p0/secrets/{secret_name}/versions/latest"
+        response = client.access_secret_version(name=name)
+        return response.payload.data.decode('UTF-8')
     
     else:
         raise ValueError(f"Unsupported APP_ENV value: {app_env}")
--- app/main.py
+++ app/main.py
@@ -4,47 +4,10 @@ import sys
 
 from flask import Flask, render_template, request
 
-current = os.path.dirname(os.path.realpath(__file__))
-parent = os.path.dirname(current)
-sys.path.append(parent)
-
+from handle_credentials import get_secret
 from handle_db_connections import create_conn, read_query
 
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
-logger.debug("Starting Flask application")
-
 app = Flask(__name__)
-
-from google.cloud import secretmanager
-
-def access_secret_version(project_id, secret_id, version_id="latest"):
-
-    """
-    Access the payload for the given secret version if one exists. The version
-    can be a version number as a string (e.g. "5") or an alias (e.g. "latest").
-    """
-    # Create the Secret Manager client.
-    client = secretmanager.SecretManagerServiceClient()
-
-    # Build the resource name of the secret version.
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
-
-    # Access the secret version.
-    response = client.access_secret_version(name=name)
-    # Return the decoded payload.
-    return response.payload.data.decode('UTF-8')
-
-# Usage example
-project_id = "durable-path-439213-p0"
-secret_id = "DB_SECRET"
-nice_value = access_secret_version(project_id, secret_id)
-
-
-'''
 
 connection = create_conn()
 
@@ -54,11 +17,9 @@ SELECT * FROM product_table;
 
 products = read_query(connection, sql_query)
 
-'''
-
 @app.route("/")
 def home():
-    return nice_value
+    return products
 
 '''
 
