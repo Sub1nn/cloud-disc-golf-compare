c4534e5b87edd9a6dce9246a44cbaedf63ad27af
Subin Khatiwada <Subinkhatiwada@gmail.com>
2025-03-25 21:15:14 +0200

    credentials handle for local setup

--- app/handle_credentials.py
+++ app/handle_credentials.py
@@ -1,22 +1,6 @@
-
 import os
-
 from dotenv import load_dotenv
-from google.cloud import secretmanager
 
 def get_secret(secret_name):
-
-    app_env = 'prod'
-    
-    if app_env == 'local':
-        load_dotenv() 
-        return os.getenv(secret_name)
-    
-    elif app_env == 'prod':
-        client = secretmanager.SecretManagerServiceClient()
-        name = f"projects/723088682483/secrets/{secret_name}/versions/latest"
-        response = client.access_secret_version(name=name)
-        return response.payload.data.decode('UTF-8')
-    
-    else:
-        raise ValueError(f"Unsupported APP_ENV value: {app_env}")
+    load_dotenv()  # Load variables from .env file
+    return os.getenv(secret_name)  # Fetch from local environment
